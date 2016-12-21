d3c887d61c769c905cdc39512f064c25e0d80357
Khaled Karam <mohamedk@greenitglobe.com>
2016-12-21 15:37:04 +0200

    Add docsrings to storlib

---
+++ docs/howtopy.md
@@ -0,0 +1,20 @@
+# Porting to python3 interface
+
+## Generate .so file guide
+* Install [nim-pymod](https://github.com/jboy/nim-pymod) by running `nimble install nim-pymod`
+* Run `python3 path/to/pmgen.py greeting.nim`
+* `.so` file will be generated that can be imported by `python3`
+
+## Examples
+```python
+import _storlib
+client = _storlib.getClientIdPy("172.17.0.1")
+
+# upload and download multiple files
+uploaded = _storlib.uploadFilesPy(client, "file1, file2", 1)
+_storlib.downloadFilesPy(client, "file1, file2", uploaded)
+
+# upload and download one file
+uploaded = _storlib.uploadFilePy(client, "file1", 1)
+_storlib.downloadFilePy(client, "file1", uploaded)
+```
--- src/storlib/storlib.nim
+++ src/storlib/storlib.nim
@@ -10,9 +10,17 @@ var
   objects = initTable[string, string]()
 
 proc getClientIdPy*(address: string = "172.17.0.1", port: int = 11213): int {.exportpy.} =
+  docstring"""Get stor clientId"""
   return stor.getClientId(address, port)
 
-proc uploadFilePy*(clientId: int, filename: string, encrypt: int): string {.exportpy.}=
+proc uploadFilePy*(clientId: int, filename: string, encrypt: int): string {.exportpy.} =
+  docstring"""
+    Upload file to pudgedb
+    @param clientId
+    @param filename: path of file that'll be uploaded
+    @param encrypt: 1 for encrypt, 0 to not encrypt
+    return: hashed string by which file can be restored
+  """
   var encryptTmp = true
   if encrypt == 0:
     encryptTmp = false
@@ -22,9 +30,22 @@ proc uploadFilePy*(clientId: int, filename: string, encrypt: int): string {.expo
   return uploadedRunes
 
 proc downloadFilePy*(clientId: int, filename: string, msg: string) {.exportpy.} =
+  docstring"""
+    Download file to file system
+    @param clientId
+    @param filename: path to which the file will be downloaded
+    @param msg: hashed string by which file can be restored
+  """
   stor.downloadFile(clientId, filename, objects[msg])
 
 proc uploadFilesPy*(clientId: int, filenames: string, encrypt: int): string {.exportpy.} =
+  docstring"""
+    Upload files to pudgedb
+    @param clientId
+    @param filenames: paths (string separated by ",") of file that'll be uploaded
+    @param encrypt: 1 for encrypt, 0 to not encrypt
+    return: hashed string by which files can be restored
+  """
   var encryptTmp = true
   if encrypt == 0:
     encryptTmp = false
@@ -35,6 +56,12 @@ proc uploadFilesPy*(clientId: int, filenames: string, encrypt: int): string {.ex
   return uploadedRunes
 
 proc downloadFilesPy*(clientId: int, filenames: string, msgs: string) {.exportpy.} =
+  docstring"""
+    Download files to file system
+    @param clientId
+    @param filenames: paths (string separated by ",") to which the files will be downloaded
+    @param msgs: hashed string by which files can be restored
+  """
   stor.downloadFiles(clientId, filenames.split(","), objects[msgs])
 
 initPyModule("_storlib", getClientIdPy, downloadFilePy, downloadFilesPy, uploadFilePy, uploadFilesPy)
